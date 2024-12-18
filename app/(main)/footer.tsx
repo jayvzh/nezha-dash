cd0903785041b8e640c3b48c11ff30d31fa460da
Jayv Zh <jayv.zh@gmail.com>
2024-12-18 13:42:47 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -10,6 +10,7 @@ export default function Footer() {
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("section_607-869_2020")}
           {new Date().getFullYear()}{" "}
+          <a href={"https://github.com/hamster1963/nezha-dash"}>NezhaDash</a>
         </section>
       </section>
     </footer>
