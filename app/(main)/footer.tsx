03785af3b3f078422b009d1a52d942a32c012262
Jayv Zh <jayv.zh@gmail.com>
2024-12-18 10:38:49 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -8,6 +8,7 @@ export default function Footer() {
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
+          {t("section_607-869_2020")}
           {new Date().getFullYear()}{" "}
         </section>
       </section>
