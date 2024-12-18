560929513902b8ed8f53fe9a49b65d6373d5bf34
Jayv Zh <jayv.zh@gmail.com>
2024-12-18 09:42:57 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -7,6 +7,7 @@ export default function Footer() {
   return (
     <footer className="mx-auto w-full max-w-5xl">
       <section className="flex flex-col">
+        /* 
         <p className="mt-3 flex gap-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("p_146-598_Findthecodeon")}{" "}
           <a
@@ -24,10 +25,11 @@ export default function Footer() {
             v{version}
           </a>
         </p>
+        */
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
           {t("section_607-869_2020")}
           {new Date().getFullYear()}{" "}
-          <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a>
+          // <a href={"https://buycoffee.top"}>{t("a_800-850_Hamster1963")}</a>
         </section>
       </section>
     </footer>
